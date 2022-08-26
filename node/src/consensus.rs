780f47f93b750c951979f3c25455c7bd099d9a0e
Fiono11 <ruipedromorais11@gmail.com>
2022-08-26 16:35:55 +0100

    WIP

--- node/src/consensus.rs
+++ node/src/consensus.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::process::id;
 use rand::prelude::SliceRandom;
 use rand::{Rng, thread_rng};
 
@@ -6,157 +7,155 @@ const NUMBER_OF_NODES: usize = 4;
 const NUMBER_OF_BYZANTINE_NODES: usize = (NUMBER_OF_NODES - 1) / 3;
 const NUMBER_OF_HONEST_NODES: usize = NUMBER_OF_NODES - NUMBER_OF_BYZANTINE_NODES;
 const QUORUM: usize = NUMBER_OF_BYZANTINE_NODES * 2 + 1;
-const TOTAL_TXS: usize = 1000;
+const TOTAL_TXS: usize = 10;
 
 fn main() {
-    println!("nodes: {}", NUMBER_OF_NODES);
-    println!("honest nodes: {}", NUMBER_OF_HONEST_NODES);
-    println!("byzantine nodes: {}", NUMBER_OF_BYZANTINE_NODES);
-    println!("quorum: {}", QUORUM);
     let mut total_confirmed = 0;
-    for _ in 0..TOTAL_TXS
-    {
-        let mut r1_votes: HashMap<usize, Vec<i32>> = HashMap::new();
-        let mut decided: HashMap<usize, Vec<i32>> = HashMap::new();
-        let mut votes = vec![];
 
-        /// Pick the initial votes of honest nodes between 0 and 1
+    for _ in 0..TOTAL_TXS {
+
+        let mut votes: HashMap<(usize, usize), Vec<i32>> = HashMap::new();
+        let mut nodes: HashMap<usize, usize> = HashMap::new();
+        let mut zero_quorum: HashMap<usize, Vec<usize>> = HashMap::new();
+        let mut one_quorum: HashMap<usize, Vec<usize>> = HashMap::new();
+        let mut decided: HashMap<usize, i32> = HashMap::new();
+        let mut excluded_nodes = vec![];
+
+        /// Initial honest votes
         for i in 0..NUMBER_OF_HONEST_NODES {
-            match decided.get(&i) {
-                Some(d) => {
-                    votes = d.clone();
-                },
-                None => {
-                    let v1 = vec![(0, 1), (1, 1)];
-                    let vote = v1.choose_weighted(&mut rand::thread_rng(), |item| item.1).unwrap().0;
-                    votes.push(vote);
-                }
+            let choices = vec![(0, 1), (1, 1)];
+            let honest_vote = choices.choose_weighted(&mut rand::thread_rng(), |item| item.1).unwrap().0;
+            let mut honest_votes = vec![];
+            let new_choices = vec![(honest_vote, 1), (-1, 1)];
+            for _ in 0..NUMBER_OF_NODES {
+                let new_honest_vote = new_choices.choose_weighted(&mut rand::thread_rng(), |item| item.1).unwrap().0;
+                honest_votes.push(new_honest_vote);
             }
+            votes.insert((i, 0), honest_votes);
+            nodes.insert(i, 0);
+            zero_quorum.insert(i, vec![]);
+            one_quorum.insert(i, vec![]);
+        }
+
+        /// Initial byzantine votes
+        for i in 0..NUMBER_OF_BYZANTINE_NODES {
+            let mut byzantine_votes = vec![];
+            let byzantine_vote = rand::thread_rng().gen_range(-1..2);
+            for _ in 0..NUMBER_OF_NODES {
+                byzantine_votes.push(byzantine_vote);
+            }
+            votes.insert((i + NUMBER_OF_HONEST_NODES, 0), byzantine_votes);
+            nodes.insert(i + NUMBER_OF_HONEST_NODES, 0);
+        }
+
+        /// Assert that all nodes have voted
+        for i in 0..NUMBER_OF_NODES {
+            assert_eq!(votes.get(&(i, 0)).unwrap().len(), NUMBER_OF_NODES);
         }
 
         loop {
-            for i in 0..NUMBER_OF_NODES {
-                if (0..NUMBER_OF_HONEST_NODES).collect::<Vec<usize>>().contains(&i) {
-                    let mut votes1: Vec<i32> = vec![];
-                    //let vote = rand::thread_rng().gen_range(0..2);
-                    //let v1 = vec![(0, 1), (1, 10)];
-                    //let vote = v1.choose_weighted(&mut rand::thread_rng(), |item| item.1).unwrap().0;
-                    /// There is a probability of a vote of an honest node to be -1 because it was lost or delayed
-                    for j in 0..NUMBER_OF_NODES {
-                        let v = vec![(votes[i], 1), (-1, 1)];
-                        let r = v.choose_weighted(&mut rand::thread_rng(), |item| item.1).unwrap().0;
-                        votes1.push(r);
-                    }
-                    r1_votes.insert(i, votes1);
-                } else {
-                    let mut votes2 = vec![];
-                    /// Votes of byzantine nodes are random and can be -1, 0 or 1
-                    for i in 0..NUMBER_OF_NODES {
-                        let vote = rand::thread_rng().gen_range(-1..2);
-                        votes2.push(vote);
-                    }
-                    r1_votes.insert(i, votes2);
-                }
-            }
-            println!("r1_votes: {:?}", r1_votes);
-            for i in 0..NUMBER_OF_NODES {
-                assert_eq!(r1_votes[&i].len(), NUMBER_OF_NODES);
-            }
-
-            let mut r2_votes = HashMap::new();
-            for i in 0..NUMBER_OF_NODES {
-                if (0..NUMBER_OF_HONEST_NODES).collect::<Vec<usize>>().contains(&i) {
-                    let mut v = r1_votes.get(&i).unwrap().clone();
-                    let mut votes3 = vec![];
-                    for j in 0..r1_votes.len() {
-                        //if i != j {
-                        let vote = r1_votes.get(&j).unwrap();
-                        votes3.push(vote[i]);
-                        //}
-                    }
-                    //println!("votes3: {:?}", votes3);
-                    let n = votes3.iter().filter(|n| **n == 1).count();
-                    let l = votes3.iter().filter(|n| **n == 0).count();
-
-                    if votes3.len() > QUORUM {
-                        if n > QUORUM {
-                            //v = vec![1, 1, 1];
-                            for i in 0..v.len() {
-                                v[i] = 1;
-                            }
-                        }
-                        //r2_votes.insert(i, vec![1, 1, 1]);
-                    } else if l > QUORUM {
-                        for i in 0..v.len() {
-                            v[i] = 0;
-                        }
-                    }
-                    //v = vec![0, 0, 0];
-                    //r2_votes.insert(i, vec![0, 0, 0]);
-                    //}
-                    //} else {
-                    //for i in 0..v.len() {
-                    //v[i] = -1;
-                    //}
-                    //v = vec![-1, -1, -1];
-                    //r2_votes.insert(i, vec![-1, -1, -1]);
-                    //}
-                    r2_votes.insert(i, v);
-                } else {
-                    let mut v1 = vec![];
-                    for i in 0..NUMBER_OF_NODES {
-                        let vote = rand::thread_rng().gen_range(-1..2);
-                        v1.push(vote);
-                        //v.push(vote);
-                    }
-                    r2_votes.insert(i, v1);
-                }
-            }
-
-            println!("r2_votes: {:?}", r2_votes);
-            for i in 0..NUMBER_OF_NODES {
-                assert_eq!(r2_votes[&i].len(), NUMBER_OF_NODES);
-            }
-
-            //let mut decided = vec![];
-            let mut confirmed = 0;
+            println!("votes: {:?}", votes);
 
             for i in 0..NUMBER_OF_HONEST_NODES {
-                let mut votes5 = vec![];
-                for j in 0..r2_votes.len() {
-                    //if i != j {
-                    let vote = r2_votes.get(&j).unwrap();
-                    votes5.push(vote[i]);
-                    //}
-                }
-                let n = votes5.iter().filter(|n| **n == 1).count();
-                let k = votes5.iter().filter(|n| **n == 0).count();
-
-                if !decided.contains_key(&i) {
-                    if votes5.len() > QUORUM {
-                        if n > 2 {
-                            println!("Node {} decided {}", i, 1);
-                            decided.insert(i, vec![1; NUMBER_OF_NODES]);
-                            confirmed += 1;
-                        } else if k > 2 {
-                            println!("Node {} decided {}", i, 0);
-                            decided.insert(i, vec![0; NUMBER_OF_NODES]);
-                        } else {
-                            println!("Node {} hasn't decided yet!", i);
+                let current_round = *nodes.get(&i).unwrap();
+                let mut zeros = 0;
+                let mut ones = 0;
+                for j in 0..NUMBER_OF_NODES {
+                    match votes.get(&(j, current_round)) {
+                        Some(votes) => {
+                            if votes[i] == 0 && !excluded_nodes.contains(&j) {
+                                zeros += 1;
+                            }
+                            if votes[i] == 1 && !excluded_nodes.contains(&j) {
+                                ones += 1;
+                            }
                         }
-                    } else {
-                        println!("Node {} hasn't decided yet!", i);
+                        None => {
+                            println!("Node {} hasn't voted in round {} yet!", j, current_round);
+                        }
+                    }
+                }
+                if zeros + ones >= QUORUM {
+                    let mut one = one_quorum.get(&i).unwrap().clone();
+                    let mut zero = zero_quorum.get(&i).unwrap().clone();
+                    if ones >= QUORUM {
+                        one.push(current_round);
+                        one_quorum.insert(i, one.clone());
+                        votes.insert((i, current_round + 1), vec![1; NUMBER_OF_NODES]);
+                        println!("Node {} voted {} in round {}", i, 1, current_round);
+                        if current_round != 0 {
+                            if one.contains(&(current_round - 1)) {
+                                decided.insert(i, 1);
+                                println!("Node {} decided {} in round {}", i, 1, current_round);
+                            }
+                        }
+                    }
+                    else {
+                        zero.push(current_round);
+                        zero_quorum.insert(i, zero.clone());
+                        votes.insert((i, current_round + 1), vec![0; NUMBER_OF_NODES]);
+                        println!("Node {} voted {} in round {}", i, 0, current_round);
+                        if current_round != 0 {
+                            if zero.contains(&(current_round - 1)) {
+                                decided.insert(i, 0);
+                                println!("Node {} decided {} in round {}", i, 0, current_round);
+                            }
+                        }
+                    }
+                    nodes.insert(i, current_round + 1);
+                }
+                else {
+                    /// Receive delayed honest votes
+                    for j in 0..NUMBER_OF_HONEST_NODES {
+                        match votes.get(&(j, current_round)) {
+                            Some(v) => {
+                                let mut new_votes = v.clone();
+                                for i in 0..new_votes.len() {
+                                    if new_votes[i] == -1 {
+                                        if new_votes.contains(&1) {
+                                            new_votes[i] = 1;
+                                        }
+                                        else {
+                                            new_votes[i] = 0;
+                                        }
+                                    }
+                                }
+                                votes.insert((j, current_round), new_votes);
+                            }
+                            None => {
+                                println!("Node {} hasn't voted in round {} yet!", j, current_round);
+                            }
+                        }
                     }
                 }
             }
+
+            /// Try to conclude
             if decided.len() == NUMBER_OF_HONEST_NODES {
                 let d = decided.get(&0).unwrap();
                 assert!(decided.iter().all(|(a, b)| b == d));
                 println!("Consensus achieved!");
-                if d[0] == 1 {
+                if d == &1 {
                     total_confirmed += 1;
                 }
                 break;
+            }
+
+            /// New byzantine votes
+            for i in 0..NUMBER_OF_BYZANTINE_NODES {
+                let current_round = *nodes.get(&(NUMBER_OF_HONEST_NODES + i)).unwrap();
+                let old_votes = votes.get(&(NUMBER_OF_HONEST_NODES + i, current_round)).unwrap();
+                if old_votes.contains(&1) && old_votes.contains(&0) {
+                    excluded_nodes.push(NUMBER_OF_HONEST_NODES + i);
+                    println!("Byzantine node {} excluded!", NUMBER_OF_HONEST_NODES + i);
+                }
+                let mut byzantine_votes = vec![];
+                let byzantine_vote = rand::thread_rng().gen_range(-1..2);
+                for _ in 0..NUMBER_OF_NODES {
+                    byzantine_votes.push(byzantine_vote);
+                }
+                votes.insert((i + NUMBER_OF_HONEST_NODES, current_round + 1), byzantine_votes);
+                nodes.insert(i + NUMBER_OF_HONEST_NODES, current_round + 1);
             }
         }
     }
